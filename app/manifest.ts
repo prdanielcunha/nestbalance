135065b6b0a9bd68f2877b692ea7907ac018c8f1
prdanielcunha <pastordanielpcunha@gmail.com>
2026-09-20 22:28:33 -0300

    feat: add app/manifest.ts

---
+++ app/manifest.ts
@@ -0,0 +1,4 @@
+import type { MetadataRoute } from 'next';
+export default function manifest(): MetadataRoute.Manifest {
+  return { name: 'NestBalance', short_name: 'NestBalance', description: 'Sua vida financeira, finalmente simples.', start_url: '/', display: 'standalone', background_color: '#F6F7F9', theme_color: '#0B1020' };
+}
