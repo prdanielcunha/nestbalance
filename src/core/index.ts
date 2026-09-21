87f92158e0bfefdb87e6abaf3bbb4043c47ab651
prdanielcunha <pastordanielpcunha@gmail.com>
2026-09-20 22:43:47 -0300

    feat: copy NestBalance foundation

---
+++ src/core/index.ts
@@ -0,0 +1,6 @@
+export * from "./types.js";
+export * from "./text-parser.js";
+export * from "./fingerprint.js";
+export * from "./summary.js";
+export * from "./installments.js";
+export * from "./evidence.js";
