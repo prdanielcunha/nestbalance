29a8cfae3dc5d352107d20e380b60c9b1b1c69c8
prdanielcunha <pastordanielpcunha@gmail.com>
2026-09-24 07:07:39 -0300

    feat(home): return member Home preferences

--- server/home.ts
+++ server/home.ts
@@ -6,6 +6,7 @@ import { categoryFromRecordAndRules, learnedCategoryRuleFromDoc, type LearnedCat
 import { normalizeProactivityPreferences } from '../src/core/proactivity.js';
 import { activeAttentionDismissals } from '../src/core/attention.js';
 import { normalizeSavingsPotAutomation } from '../src/core/savings-pot-automation.js';
+import { normalizeHomePreferences } from '../src/core/home-preferences.js';
 
 function error(res:Response,status:number,code:string){
   return res.status(status).json({ok:false,error:code});
@@ -206,6 +207,7 @@ export async function getHomeData(req:Request,res:Response){
       savingsPots:visibleDocs(savingsPots.docs,user.uid).map(savingsPotDto),
       cardSnapshots:visibleDocs(cardSnapshots.docs,user.uid).map(cardSnapshotDto),
       proactivityPreferences:normalizeProactivityPreferences(currentMember.proactivityPreferences),
+      homePreferences:normalizeHomePreferences(currentMember.homePreferences),
       dismissedAttentionKeys:activeAttentionDismissals(attentionDismissalsSnap.docs.map(doc=>doc.data()),user.uid,Date.now()),
       dismissedRecurrenceKeys:visibleDocs(recurrenceDismissals.docs,user.uid)
         .map(doc=>{
